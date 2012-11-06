5f527818711dae93b5a338f309aad094a3148023
jfabrix <fabrix.jf@gmail.com>
2012-11-06 11:47:50 +0100

    Dont compress binaries on release, added inno setup file

--- singoutstrong/deploy.bat
+++ singoutstrong/deploy.bat
@@ -2,8 +2,8 @@ IF EXIST .\Dist rd .\Dist /S /Q
 xcopy .\jdksmidi\release\*.dll .\Dist /I
 xcopy .\soscore\release\*.dll .\Dist /I
 xcopy .\singoutstrong\release\*.exe .\Dist /I
-upx .\Dist\*.* --best -q
 xcopy .\singoutstrong\skins .\Dist\skins /E /I
 xcopy .\libs\dll\release\*.* .\Dist /E /I
 xcopy .\singoutstrong\samples .\Dist\samples /E /I
 xcopy .\singoutstrong\docs .\Dist\docs /E /I
+signtool.exe sign /a .\Dist\singoutstrong.exe
